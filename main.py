a9f16f1e742a8e26c9db28060ed6930f773f68da
Masa512411 <masa1717@gol.com>
2018-01-06 16:46:00 +0900

    deploy_second

--- main.py
+++ main.py
@@ -6,33 +6,36 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
-import ramdom
+import random
 
 app = Flask(__name__)
 
 @app.route("/webhook",methods=['POST'])
 def webhook():
     req = request.get_json()
-    novels = req["parameters"]["novels"]
+    novels = req["result"]["parameters"]["novels"]
     quotations = json.load(open("quotations.json",encoding="utf-8_sig"))
 
     if novels == "羊をめぐる冒険":
-        speech = random.choice(quotations.get("羊をめぐる冒険"))
-    else if novels == "風の歌を聞け":
-        speech = random.choice(quotations.get("風の歌を聴け")) 
-    else if novels == "ダンスダンスダンス":
-        speech = random.choice(quotations.get("ダンスダンスダンス")) 
-    else if novels == "ノルウェイの森":
-        speech = random.choice(quotations.get("ノルウェイの森")) 
-    else if novels == "ねじ巻き鳥クロニクル":
-        speech = random.choice(quotations.get("ねじ巻き鳥")) 
-    else if novels == "世界の終わりとハードボイルドワンダーランド":
-        speech = random.choice(quotations.get("セカオワ"))
+        speech = random.choice(list(quotations.get("羊をめぐる冒険").values()))
+    elif novels == "風の歌を聴け":
+        speech = random.choice(list(quotations.get("風の歌を聴け").values())) 
+    elif novels == "ダンスダンスダンス":
+        speech = random.choice(list(quotations.get("ダンスダンスダンス").values()))
+    elif novels == "ノルウェイの森":
+        speech = random.choice(list(quotations.get("ノルウェイの森").values()))
+    elif novels == "ねじ巻き鳥クロニクル":
+        speech = random.choice(list(quotations.get("ねじ巻き鳥").values()))
+    elif novels == "世界の終わりとハードボイルドワンダーランド":
+        speech = random.choice(list(quotations.get("セカオワ").values()))
 
-     req["result"]["fulfillment"]["speech"] = speech
+    req["result"]["fulfillment"]["speech"] = speech
 
-     return jsonify(req)
+    return jsonify(req)
         
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)
-    app.run(debug=False,port=port,host='0.0.0.0')
+    port = int(os.getenv("PORT", 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run()
