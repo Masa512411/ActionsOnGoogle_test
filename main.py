7b58d9fdd557a87f5598222184d5a0a51774ecaf
Masa512411 <masa1717@gol.com>
2018-01-06 17:58:31 +0900

    [fix]main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
 
     print("Starting app on port %d" % port)
 
